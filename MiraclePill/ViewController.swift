c358489ef4034e47fbeb8cf9675f5c8f2814193c
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-11-26 16:24:05 -0800

    Initial Commit

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Macbook Air on 11/26/16.
+//  Copyright © 2016 David Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.purple
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
